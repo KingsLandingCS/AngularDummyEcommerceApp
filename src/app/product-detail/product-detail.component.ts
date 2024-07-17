a117b4aeb32eab09cb203bbde63aa86a87b324e5
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-07-17 05:27:49 +0500

    learning

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
+  product: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get("id")!;
+    this.apiService.getProductsById(id).subscribe(product =>{
+      this.product = product;
+    });
+  }
+
+  addToCart(): void{
+    this.apiService.addToCart({productId: this.product.id, quantity: 1}).subscribe(() => {
+      alert("Product added to cart");
+    })
+  }
 }
